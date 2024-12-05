e94f45992edc462bcd7808b4220902530d6a85a5
kihyeoon <kihyeoon@gmail.com>
2024-12-05 16:33:33 +0900

    🔍 JSON-LD 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { GoogleAnalytics } from "@next/third-parties/google";
 import type { Metadata, Viewport } from "next";
 import { Alata } from "next/font/google";
 import localFont from "next/font/local";
+import { Organization, WithContext } from "schema-dts";
 import Footer from "@/components/common/footer/footer";
 import Header from "@/components/common/header";
 import { Toaster } from "@/components/ui/toaster";
@@ -19,6 +20,32 @@ const alata = Alata({
   variable: "--font-alata",
   weight: "400",
 });
+
+const jsonLd: WithContext<Organization> = {
+  "@context": "https://schema.org",
+  "@type": "Organization",
+  name: "Wherever",
+  alternateName: "주식회사 웨어에버",
+  url: process.env.SITE_URL,
+  logo: `${process.env.SITE_URL}/apple-icon.png`,
+  description: "어디서나, 어디든지, 웨어에버",
+  address: {
+    "@type": "PostalAddress",
+    streetAddress: "중앙로48길 10",
+    addressLocality: "양천구",
+    addressRegion: "서울특별시",
+    addressCountry: "KR",
+  },
+  contactPoint: {
+    "@type": "ContactPoint",
+    telephone: "070-7801-3030",
+    contactType: "customer service",
+    email: "contact@where-ever.kr",
+  },
+  sameAs: ["https://instagram.com/wherever___official/"],
+  areaServed: "KR",
+  knowsAbout: ["플레이스마케팅", "SNS마케팅", "바이럴마케팅", "브랜딩"],
+};
 
 export const metadata: Metadata = {
   title: "Wherever",
@@ -83,6 +110,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko-KR" className="dark">
+      <head>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+        />
+      </head>
       <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID as string} />
       <body
         className={`${pretendard.variable} ${alata.variable} font-[family-name:var(--font-pretendard),var(--font-alata)] antialiased`}
